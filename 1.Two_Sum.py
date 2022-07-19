0fe3fad57f57a1edd321d1ffb9419325de5ca61a
Nisala Premathilake <nisalapre@gmail.com>
2022-07-20 00:32:40 +0530

    Update 1.Two_Sum.py

--- 1.Two_Sum.py
+++ 1.Two_Sum.py
@@ -1,3 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i in range(len(nums)):
+            hashMap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashMap:
+                if i != hashMap[complement]:
+                    return [i, hashMap[complement]]
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
